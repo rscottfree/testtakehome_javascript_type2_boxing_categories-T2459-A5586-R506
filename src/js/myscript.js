73ab05cdb4ee725c6c3eab574d900b3435a10e38
Ryan <rscottfree@gmail.com>
2022-02-26 07:28:56 -0800

    Revert "Test without async/await"
    This reverts commit b1e915b5568546203dfe60efe13e90f6e7de7d08. Both
    methods fail. The table looks correctly populated to me based on the
    JSON reponse.

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,41 +1,49 @@
-$(document).ready(function() {
-    fetch('https://www.justcode.com/dataservice/api/boxingCategory/list').then(response => {
-        if (response && response.ok) {
-            return response.json() ?? [];
-        }
-
-        return Promise.resolve([]);
-    }).then(boxingCategories => {
-        const tableBody = document.querySelector('#mytable tbody');
-        if (!tableBody) {
-            return;
-        }
-
-        tableBody.innerHTML = '';
-
-        console.log('cats', boxingCategories);
-        boxingCategories.forEach(category => {
-            const row = document.createElement('tr');
-
-            const cellId = document.createElement('td');
-            cellId.innerText = category.id;
-            const cellCategoryName = document.createElement('td');
-            cellCategoryName.innerText = category.categoryName;
-            const cellUpperLimitInPound = document.createElement('td');
-            cellUpperLimitInPound.innerText = category.upperLimitInPound;
-            const cellUpperLimitInKG = document.createElement('td');
-            cellUpperLimitInKG.innerText = category.upperLimitInKG;
-            const cellModifiedOn = document.createElement('td');
-            // TODO: Use library to parse date string, and display localized date/time
-            cellModifiedOn.innerText = category.modifiedOn;
-
-            row.append(cellId);
-            row.append(cellCategoryName);
-            row.append(cellUpperLimitInPound);
-            row.append(cellUpperLimitInKG);
-            row.append(cellModifiedOn);
-
-            tableBody.appendChild(row);
-        });
-    });
+$(document).ready(async function() {
+    const boxingCategories = await getRemoteData();
+    updateTable(boxingCategories);
 });
+
+/**
+ * Fetch boxying categories from API and return result or empty array.
+ * @returns An array of boxing categories. Non-null.
+ */
+async function getRemoteData() {
+    const response = await fetch('https://www.justcode.com/dataservice/api/boxingCategory/list');
+    if (response && response.ok) {
+        return response.json();
+    }
+    return [];
+}
+
+function updateTable(boxingCategories) {
+    const tableBody = document.querySelector('#mytable tbody');
+    if (!tableBody) {
+        return;
+    }
+
+    tableBody.innerHTML = '';
+
+    boxingCategories.forEach(category => {
+        const row = document.createElement('tr');
+
+        const cellId = document.createElement('td');
+        cellId.innerText = category.id;
+        const cellCategoryName = document.createElement('td');
+        cellCategoryName.innerText = category.categoryName;
+        const cellUpperLimitInPound = document.createElement('td');
+        cellUpperLimitInPound.innerText = category.upperLimitInPound;
+        const cellUpperLimitInKG = document.createElement('td');
+        cellUpperLimitInKG.innerText = category.upperLimitInKG;
+        const cellModifiedOn = document.createElement('td');
+        // TODO: Use library to parse date string, and display localized date/time
+        cellModifiedOn.innerText = category.modifiedOn;
+
+        row.append(cellId);
+        row.append(cellCategoryName);
+        row.append(cellUpperLimitInPound);
+        row.append(cellUpperLimitInKG);
+        row.append(cellModifiedOn);
+
+        tableBody.appendChild(row);
+    });
+}
