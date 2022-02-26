678e3e17488e73817394ae17377b054b998b3400
Ryan <rscottfree@gmail.com>
2022-02-26 07:17:54 -0800

    Fetch categories and display table rows

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,7 +1,49 @@
-console.log('Welcome to HackerU-JustCode Assignment');
-$(document).ready(function() {
-    getRemoteData();
+$(document).ready(async function() {
+    const boxingCategories = await getRemoteData();
+    updateTable(boxingCategories);
 });
-function getRemoteData() {
-    // write your code here to fetch data from api.
+
+/**
+ * Fetch boxying categories from API and return result or empty array.
+ * @returns An array of boxing categories. Non-null.
+ */
+async function getRemoteData() {
+    const response = await fetch('https://www.justcode.com/dataservice/api/boxingCategory/list');
+    if (response && response.ok) {
+        return response.json();
+    }
+    return [];
+}
+
+function updateTable(boxingCategories) {
+    const tableBody = document.querySelector('#mytable tbody');
+    if (!tableBody) {
+        return;
+    }
+
+    tableBody.innerHTML = '';
+
+    boxingCategories.forEach(category => {
+        const row = document.createElement('tr');
+
+        const cellId = document.createElement('td');
+        cellId.innerText = category.id;
+        const cellCategoryName = document.createElement('td');
+        cellCategoryName.innerText = category.categoryName;
+        const cellUpperLimitInPound = document.createElement('td');
+        cellUpperLimitInPound.innerText = category.upperLimitInPound;
+        const cellUpperLimitInKG = document.createElement('td');
+        cellUpperLimitInKG.innerText = category.upperLimitInKG;
+        const cellModifiedOn = document.createElement('td');
+        // TODO: Use library to parse date string, and display localized date/time
+        cellModifiedOn.innerText = category.modifiedOn;
+
+        row.append(cellId);
+        row.append(cellCategoryName);
+        row.append(cellUpperLimitInPound);
+        row.append(cellUpperLimitInKG);
+        row.append(cellModifiedOn);
+
+        tableBody.appendChild(row);
+    });
 }
