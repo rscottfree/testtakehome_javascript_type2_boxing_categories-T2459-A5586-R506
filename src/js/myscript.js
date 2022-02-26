b1e915b5568546203dfe60efe13e90f6e7de7d08
Ryan <rscottfree@gmail.com>
2022-02-26 07:25:37 -0800

    Test without async/await
    Code check is returning failure despite the table being populated. The
    code checker may be looking for basic promises, not async/await?

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,49 +1,41 @@
-$(document).ready(async function() {
-    const boxingCategories = await getRemoteData();
-    updateTable(boxingCategories);
-});
+$(document).ready(function() {
+    fetch('https://www.justcode.com/dataservice/api/boxingCategory/list').then(response => {
+        if (response && response.ok) {
+            return response.json() ?? [];
+        }
 
-/**
- * Fetch boxying categories from API and return result or empty array.
- * @returns An array of boxing categories. Non-null.
- */
-async function getRemoteData() {
-    const response = await fetch('https://www.justcode.com/dataservice/api/boxingCategory/list');
-    if (response && response.ok) {
-        return response.json();
-    }
-    return [];
-}
+        return Promise.resolve([]);
+    }).then(boxingCategories => {
+        const tableBody = document.querySelector('#mytable tbody');
+        if (!tableBody) {
+            return;
+        }
 
-function updateTable(boxingCategories) {
-    const tableBody = document.querySelector('#mytable tbody');
-    if (!tableBody) {
-        return;
-    }
+        tableBody.innerHTML = '';
 
-    tableBody.innerHTML = '';
+        console.log('cats', boxingCategories);
+        boxingCategories.forEach(category => {
+            const row = document.createElement('tr');
 
-    boxingCategories.forEach(category => {
-        const row = document.createElement('tr');
+            const cellId = document.createElement('td');
+            cellId.innerText = category.id;
+            const cellCategoryName = document.createElement('td');
+            cellCategoryName.innerText = category.categoryName;
+            const cellUpperLimitInPound = document.createElement('td');
+            cellUpperLimitInPound.innerText = category.upperLimitInPound;
+            const cellUpperLimitInKG = document.createElement('td');
+            cellUpperLimitInKG.innerText = category.upperLimitInKG;
+            const cellModifiedOn = document.createElement('td');
+            // TODO: Use library to parse date string, and display localized date/time
+            cellModifiedOn.innerText = category.modifiedOn;
 
-        const cellId = document.createElement('td');
-        cellId.innerText = category.id;
-        const cellCategoryName = document.createElement('td');
-        cellCategoryName.innerText = category.categoryName;
-        const cellUpperLimitInPound = document.createElement('td');
-        cellUpperLimitInPound.innerText = category.upperLimitInPound;
-        const cellUpperLimitInKG = document.createElement('td');
-        cellUpperLimitInKG.innerText = category.upperLimitInKG;
-        const cellModifiedOn = document.createElement('td');
-        // TODO: Use library to parse date string, and display localized date/time
-        cellModifiedOn.innerText = category.modifiedOn;
+            row.append(cellId);
+            row.append(cellCategoryName);
+            row.append(cellUpperLimitInPound);
+            row.append(cellUpperLimitInKG);
+            row.append(cellModifiedOn);
 
-        row.append(cellId);
-        row.append(cellCategoryName);
-        row.append(cellUpperLimitInPound);
-        row.append(cellUpperLimitInKG);
-        row.append(cellModifiedOn);
-
-        tableBody.appendChild(row);
+            tableBody.appendChild(row);
+        });
     });
-}
+});
