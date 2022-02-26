13417318595ea46f599dc86834640b9af6d3ede2
justcode-system <an@justcode.com>
2022-02-26 14:39:58 +0000

    Initial commit

---
+++ src/js/myscript.js
@@ -0,0 +1,7 @@
+console.log('Welcome to HackerU-JustCode Assignment');
+$(document).ready(function() {
+    getRemoteData();
+});
+function getRemoteData() {
+    // write your code here to fetch data from api.
+}
